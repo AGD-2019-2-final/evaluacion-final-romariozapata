35e11786df1053b5c282a59a3f5188bef1dc9836
JasminMarin <31749284+JasminMarin@users.noreply.github.com>
2020-02-21 17:05:29 -0500

    Update mapper.py

--- 01-hadoop-50/q02-10/mapper.py
+++ 01-hadoop-50/q02-10/mapper.py
@@ -2,3 +2,11 @@ import sys
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+infile = sys.stdin
+#next(infile) # skip first line of input file
+for line in infile:
+    line = line.split(',')
+    amount = line[4]
+    purpose = line[3]
+    #line = line.split(',', 2)
+    sys.stdout.write("{}\t{}\n".format(purpose,amount))
