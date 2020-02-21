4f9b69d7b38c32652746c268fa7c8b04fc92dd14
JasminMarin <31749284+JasminMarin@users.noreply.github.com>
2020-02-21 17:05:58 -0500

    Update mapper.py

--- 01-hadoop-50/q05-10/mapper.py
+++ 01-hadoop-50/q05-10/mapper.py
@@ -2,3 +2,11 @@ import sys
 #
 #  >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+# input comes from STDIN (standard input)
+for line in sys.stdin:
+    line = line.strip()
+    line = line.split(' ')
+    temp = line[3]
+    temp = temp.split('-')
+    temp = temp[1]
+    sys.stdout.write("{},1\n".format(temp))
